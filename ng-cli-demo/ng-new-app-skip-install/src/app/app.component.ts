395de148ae0a167d7773a6f403014d229462db2d
neilyoTracer <kk_cyndi@126.com>
2019-05-07 18:24:23 +0800

    update

--- ng-cli-demo/ng-new-app-skip-install/src/app/app.component.ts
+++ ng-cli-demo/ng-new-app-skip-install/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
+import { RouterOutlet } from '@angular/router';
+import { slideInAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  animations: [
+    slideInAnimation
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
-  title = 'ng-test';
+  @HostBinding('@.disabled')
+  public animationsDisabled = false;
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+
+  toggleAnimations() {
+    this.animationsDisabled = !this.animationsDisabled;
+  }
 }
